79baaf1bb1e9146d59619a6e430d253c2b7e376d
MLMikuLing <1936838210@qq.com>
2018-09-05 00:42:43 +0800

    Socket

---
+++ Client/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReceiveEvent.Instance.AddEvent(cw);
+            ConnServer.Initial("127.0.0.1", 9002);
+            ConnServer.Instance.Conn();
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                ConnServer.Instance.SendMessage(str);
+            }
+        }
+        static void cw(string n)
+        {
+            Console.WriteLine(n);
+        }
+    }
+
+    /// <summary>
+    /// 连接服务器
+    /// </summary>
+    public class ConnServer
+    {
+        public static ConnServer Instance;
+        private Socket client;
+        private string ip;
+        private int port;
+        private IPAddress ipaddress;
+        private byte[] result = new byte[1024];
+        /// <summary>
+        /// 初始化参数
+        /// </summary>
+        private ConnServer(string ip, int port)
+        {
+            this.ip = ip;
+            this.port = port;
+            ipaddress = IPAddress.Parse(ip);
+            client = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+        }
+        /// <summary>
+        /// 连接服务器
+        /// </summary>
+        public void Conn()
+        {
+            if (!client.Connected)
+            {
+                client.Connect(new IPEndPoint(ipaddress, port));
+                Thread startReceive = new Thread(ReceiveMessage);
+                startReceive.IsBackground = true;
+                startReceive.Start();
+            }
+        }
+        /// <summary>
+        /// 接收来自服务器的信息
+        /// </summary>
+        public void ReceiveMessage()
+        {
+            while (true)
+            {
+                int mesLength = client.Receive(result);
+                ReceiveEvent.Instance.play(Encoding.UTF8.GetString(result, 0, mesLength));
+            }
+        }
+        /// <summary>
+        /// 向服务器发送信息
+        /// </summary>
+        public void SendMessage(string info)
+        {
+            client.Send(Encoding.UTF8.GetBytes(info));
+        }
+        /// <summary>
+        /// 初始化参数
+        /// </summary>
+        public static void Initial(string ip, int port)
+        {
+            Instance = new ConnServer(ip, port);
+        }
+    }
+
+    /// <summary>
+    /// 添加事件，有信息传入可直接调取方法
+    /// </summary>
+    public class ReceiveEvent
+    {
+        private static ReceiveEvent instance;
+        public static ReceiveEvent Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ReceiveEvent();
+                return instance;
+            }
+        }
+        public delegate void Del(string info);
+        private Del Receive;
+        /// <summary>
+        /// 添加一个方法到委托中
+        /// </summary>
+        public void AddEvent(Del e)
+        {
+            Receive += e;
+        }
+        /// <summary>
+        /// 执行委托中的事件
+        /// </summary>
+        public void play(string info)
+        {
+            Receive(info);
+        }
+    }
+
+}
---
+++ SimpleSocket/Server.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace SimpleSocket
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            ServerSide.Initial("127.0.0.1", 9002);
+            Console.ReadLine();
+        }
+    }
+    public class ServerSide
+    {
+        private Dictionary<int, Client> SaveClientConn;
+        private int clientID;
+        private Thread listenClientConn;
+        private Socket server;
+        private IPAddress ipAddress;
+        private static ServerSide Instance;
+        private ServerSide(string ip, int port)
+        {
+            DeleEvent.sendMessage += Write;
+            SaveClientConn = new Dictionary<int, Client>();
+            clientID = 0;
+            ipAddress = IPAddress.Parse(ip);
+            server = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            server.Bind(new IPEndPoint(ipAddress, port));
+            server.Listen(10);
+            listenClientConn = new Thread(ListenConn);
+            listenClientConn.IsBackground = true;
+            listenClientConn.Start();
+        }
+        public static void Initial(string ip, int port)
+        {
+            Instance = new ServerSide(ip, port);
+        }
+
+        private void ListenConn()
+        {
+            while (true)
+            {
+                Socket client = server.Accept();
+                Client c = new Client(client, clientID);
+                SaveClientConn.Add(clientID, c);
+                clientID++;
+            }
+        }
+
+        private void Write(string clientMes)
+        {
+            Console.WriteLine(clientMes);
+        }
+    }
+    class Client
+    {
+        private Socket client;
+        private int clientID;
+        private Thread receive;
+        private byte[] result = new byte[1024];
+        public Client(Socket client, int clientID)
+        {
+            DeleEvent.AddEvent(SendMessage);
+            this.client = client;
+            this.clientID = clientID;
+            receive = new Thread(ReceiveMessage);
+            receive.IsBackground = true;
+            receive.Start();
+        }
+        private void ReceiveMessage()
+        {
+            while (true)
+            {
+                try { 
+                    int length = client.Receive(result);
+                    Console.WriteLine("Client-{0}:{1}", clientID, Encoding.UTF8.GetString(result, 0, length));
+                    DeleEvent.sendMessage -= SendMessage;
+                    DeleEvent.sendMessage(client + ":"+Encoding.UTF8.GetString(result, 0, length));
+                    DeleEvent.sendMessage += SendMessage;
+                    SendMessage("Server:我知道了,你可以闭嘴了");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Client-{0}关闭连接:Info-{1}", clientID, ex.Message);
+                    break;
+                }
+            }
+        }
+        private void SendMessage(string info)
+        {
+            client.Send(Encoding.UTF8.GetBytes(info));
+        }
+    }
+
+    class DeleEvent
+    {
+        public delegate void dele(string info);
+        public static dele sendMessage;
+               
+        public static void AddEvent(dele e)
+        {
+            sendMessage += e;
+        }
+    }
+
+
+}
